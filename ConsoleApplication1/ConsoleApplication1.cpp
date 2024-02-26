1a4d29087f4619cd87f83e1b67094dd6f4323486
verss21 <arjunafrizal01@gmail.com>
2024-02-26 11:27:15 +0700

    add variable jejari and luas

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
+	//begin
+		// numeric nJejari, nLuas
+		// display ' Masukan jejari= '
+		// accept nJejari
+		// compute nLuas = 3,14 * r * r
+		// display 'Luasnya= ' + nLuas
+	//end
+
+	double nJejari, nLuas;
+	cout << "Masukan jejari = ";
+
 
 }
 
